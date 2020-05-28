319a0d411aa26602ff9d030c93aff6550aabf301
Daniel Lopes <ddanielssoares@gmail.com>
2020-05-28 02:13:10 -0300

    Adicionado método que esconde a palavra
    Foi adicionado o método "esconder_palavra( )" que esconde a palavra em caracteres "-" e revela somente as letras descobertas.

--- Jogo_da_forca_v1/jogoforca_v1.py
+++ Jogo_da_forca_v1/jogoforca_v1.py
@@ -70,16 +70,20 @@ O   |
      |
 =========''']
 
-versao = '1.0'
+versao = '1.1'
 
 
 # Classe
 class Boneco:
+    # Atributos
     palavra = str()
     palavraOrigem = str()
+    tamanho = int()
+    statusGame = bool()
+
     # Método Construtor
     def __init__(self, palavr):
-        palavraOrigem = palavr[0]
+        self.palavraOrigem = palavr[0]
         self.palavra = palavr[0]
         tamanho = len(self.palavra)
         statusGame = False
@@ -91,13 +95,15 @@ class Boneco:
             return True
         else:
             return False
+
     # Método para verificar se o jogo terminou
     def fim_jogo(self, acertos, erros):
-        if (erros == 5) or (acertos == len(self.palavra)):
+        if erros == 6:
+            return True
+        elif acertos == len(self.palavra):
             return True
         else:
             return False
-
 
     # Método para verificar se o jogador venceu
     def vitoria(self):
@@ -106,24 +112,22 @@ class Boneco:
         else:
             return False
 
-    # Método para não mostrar a letra no board
-    def esconder_palavra(self, controle):
-        if controle:
-            print('show')
-        else:
-            espaco = '_' * len(self.palavra)
-            print('Palavra: '+'_' * len(self.palavra))
+    # Método para não mostrar a letra, e mostrar apenas quando o usuário acertar
+    def esconder_palavra(self):
+        espaco = ['_' for n in self.palavra]
+        lista = [self.palavraOrigem[n] for n in range(0, len(espaco))]
+        for n in range(0, len(espaco)):
+            if self.palavra[n] == '0':
+                espaco[n] = lista[n]
+        espaco = ''.join(espaco)
+        print('Palavra: ' + espaco)
 
     # Método para checar o status do game e imprimir o board na tela
     def status_jogo(self, acertos, erros):
         if acertos == len(self.palavra):
             self.statusGame = True
-        elif erros == 5:
+        elif erros > 6:
             self.statusGame = False
-
-
-
-
 
 # Função para ler uma palavra de forma aleatória do banco de palavras
 def rand_palavra():
@@ -131,21 +135,18 @@ def rand_palavra():
         banco = bancoPalavras.readlines()
     return banco[random.randint(0, len(banco) - 1)].split()
 
-
 # Função Main - Execução do Programa
 def main():
     # Boas Vindas ao jogo:
     print('Bem vindo ao Jogo da Forca v{}\n'.format(versao) +
           'Desenvolvido por Daniel Lopes\n')
 
-    # Objeto do tipo Boneco
+    # Objeto do tipo Boneco recebe a palavra randomica
     game = Boneco(rand_palavra())
 
     # Variáveis de controle
     acertos = 0
     erros = 0
-
-    acertoBin = False
 
     # Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
     while True:
@@ -156,26 +157,22 @@ def main():
         else:
             print(forca_img[erros])
             print('Acertos: {0} Erros: {1}\n'.format(acertos, erros) + '='*10)
-            if acertoBin:
-                game.esconder_palavra(True)
-            else:
-                game.esconder_palavra(False)
+            game.esconder_palavra()
 
         # Solicita a letra para o jogador
         letraInput = input('\nDigite uma letra: ')
 
-        # Debug
+        # Código para testes
         if letraInput == 'adm_resposta':
             print(game.palavra)
             continue
 
         # Verifica se a letra está correta
         if game.advinha(letraInput):
-            acertos += 1
+            acertos += game.palavra.count(letraInput)
             game.palavra = game.palavra.replace(letraInput, "0")
         else:
             erros += 1
-
 
     # Verifica o status do jogo
     game.status_jogo(acertos, erros)
@@ -183,14 +180,13 @@ def main():
     # De acordo com o status, imprime mensagem na tela para o usuário
     if game.vitoria():
         print('\nParabéns! Você venceu!!')
+        print('A palavra era {}.'.format(game.palavraOrigem))
     else:
         print('\nGame over! Você perdeu.')
         print('A palavra era {}.'.format(game.palavraOrigem))
 
     print('\nFoi bom jogar com você! Agora vá estudar!\n')
 
-
 # Executa o programa
 if __name__ == "__main__":
     main()
-
